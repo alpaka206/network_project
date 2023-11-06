d212da8e620aaabdca45e2b2d9b8d4aac25fc62d
winterizcoming <iswpark99@gmail.com>
2023-11-06 17:05:11 +0900

    Parking Lost State 확인 클래스 생성

--- src/functions/CheckStateFunc.java
+++ src/functions/CheckStateFunc.java
@@ -1,5 +1,26 @@
 package functions;
+import components.StateLabel;
+import java.util.List;
 
 public class CheckStateFunc {
+    public static String checkFloorState(Boolean[] parkingLots) {
+        int totalSpaces = parkingLots.length;
+        int filledSpaces = 0;
 
+        for (int i = 0; i < totalSpaces; i++) {
+            if (parkingLots[i]) {
+                filledSpaces++;
+            }
+        }
+
+        double occupancyRate = (double) filledSpaces / totalSpaces;
+
+        if (filledSpaces == totalSpaces) { 
+        	return "만차";
+        } else if (occupancyRate >= 0.5) {
+        	return "혼잡";
+        } else {
+        	return "여유";
+        }
+    }
 }
