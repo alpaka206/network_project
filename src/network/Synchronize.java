53526130c6277f172696e2250d7aa73cddefb50a
greensnapback0229 <smdmim@gmail.com>
2023-11-15 16:40:57 +0900

    feat. Synchronize UDP로 변경

--- src/network/Synchronize.java
+++ src/network/Synchronize.java
@@ -1,25 +1,56 @@
 package network;
 
-import java.io.*;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class Synchronize {
-    private int floor;
+	private int floor;
+	private int receiveArray[][] = new int[4][16];
 
-    public Synchronize(int floor) {
-        this.floor = floor;
-    }
+	private Boolean[][] parkingLots = new Boolean[4][16];
+	private Boolean[][] adminBlockState = new Boolean[4][16];
 
-    
-    public void sendRequestToServer() {
-        try(Socket socket = new Socket("server address", 12345);
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        	BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-            int flag = floor * 10 + 4;
-            writer.println(flag);
-            String result = reader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public Synchronize(int floor) {
+		this.floor = floor;
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 16; j++) {
+				parkingLots[i][j] = false;
+				adminBlockState[i][j] = true;
+			}
+		}
+	}
+
+	public void receiveCurrentState(Boolean[][] parkingLots, Boolean[][] adminBlockState) {
+		try {
+			int port = 8080; // 포트 번호 설정
+			int flag, len;
+			DatagramSocket serverSocket = new DatagramSocket(port);
+
+			byte[] receiveData = new byte[100];
+
+			while (true) {
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				serverSocket.receive(receivePacket);
+				flag = ByteBuffer.wrap(Arrays.copyOfRange(receivePacket.getData(), 0, 4)).getInt();
+				len = ByteBuffer.wrap(Arrays.copyOfRange(receivePacket.getData(), 4, 8)).getInt();
+
+				for (int i = 1; i < 4; i++) {
+					for (int j = 0; j < 16; j++) {
+						int n = ByteBuffer.wrap(Arrays.copyOfRange(receivePacket.getData(), 8 + i * 4, 12 + 4 * i))
+							.getInt();
+
+						if (n == 1)
+							parkingLots[i][j] = true;
+						else if (n == 2)
+							adminBlockState[i][j] = false;
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
