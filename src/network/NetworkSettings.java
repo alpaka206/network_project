7215674f28d7a71f0ac7b63d7ba573d111a03613
alpaka206 <gyuwon05@gmail.com>
2023-11-20 19:15:01 +0900

    chore. ignore 적용 테스트_ 2

--- src/network/NetworkSettings.java
+++ src/network/NetworkSettings.java
@@ -3,5 +3,5 @@ package network;
 public class NetworkSettings {
 	public final static int portNum = 54254;
 
-	public final static String srvIpAddr = "172.20.10.1";
+	public final static String srvIpAddr = "172.20.10.3";
 }
